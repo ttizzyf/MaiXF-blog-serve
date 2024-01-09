317af447182ecab2c4ca1a03cd8e2f92fb7dad5a
wjfe <1374144742@qq.com>
2024-01-09 23:23:20 +0800

    访客记录接口完成，ip地址解析待续

--- utils/visitor.js
+++ utils/visitor.js
@@ -1,5 +1,3 @@
-const bcrypt = require("bcryptjs");
-const request = require("request");
 const visitorModel = require("../models/w1/blog/visitor_model.js");
 const sequelize = require("../db/mysqlConnection.js");
 const UAParser = require("ua-parser-js");
@@ -14,34 +12,91 @@ const logger = require("./logger.js");
  * @param { Object } body 请求体
  * @param { string } ip IP地址
  * @param { Object } params 请求
- * @returns {Promise} true/false 两个密码是否相同
+ * @returns {Promise} 记录成功
  */
-exports.visitorRecord = (req, body, ip, params) => {
-  let userAgent = new UAParser(req.headers["user-agent"]);
-  let visitor = {
-    nickname: req.body.nickname || "visitor",
-    platform: userAgent.getBrowser().name + ".v" + userAgent.getBrowser().major,
-    os: os.type() + os.release() + " " + os.arch(),
-    type: params.type, //1 admin 0 client
-    userIp: ip,
-    loginLocation:
-      JSON.parse(body).code === 200
-        ? JSON.parse(body).data.country +
-          "-" +
-          JSON.parse(body).data.region +
-          "-" +
-          JSON.parse(body).data.city
-        : JSON.parse(body).msg,
-  };
-  const start = new Date();
-  visitorModel
-    .create(visitor)
-    .then(() => {
-      const ms = new Date() - start;
-      logger.info(`访客：${ip} : ${req.method} ${req.url} - ${ms}ms`);
-    })
-    .catch((error) => {
-      const ms = new Date() - start;
-      logger.error(`访客记录错误：${ip} : ${req.method} ${req.url} - ${ms}ms`);
-    });
+exports.visitorRecord = (req, body, ip) => {
+  return new Promise((resolve, reject) => {
+    let userAgent = new UAParser(req.headers["user-agent"]);
+    let visitor = {
+      nickname: req.body.nickname || "visitor",
+      platform:
+        userAgent.getBrowser().name + ".v" + userAgent.getBrowser().major,
+      os: os.type() + os.release() + " " + os.arch(),
+      userType: body.type, //0 admin 1 manage
+      userIp: ip,
+      loginLocation:
+        body.code === 200
+          ? body.data.country + "-" + body.data.region + "-" + body.data.city
+          : body.msg,
+    };
+    console.log(ip);
+    const start = new Date();
+    visitorModel
+      .create(visitor)
+      .then((msg) => {
+        const ms = new Date() - start;
+        let ask = `访客：${ip} : ${req.method} ${req.url} - ${ms}ms`;
+        logger.info(ask);
+        resolve({ type: 1, ask });
+      })
+      .catch((error) => {
+        const ms = new Date() - start;
+        let ask = `访客记录错误：${ip} : ${req.method} ${req.url} - ${ms}ms`;
+        logger.error(ask);
+        reject({ type: 0, ask });
+      });
+  });
+};
+
+/**
+ * visitorFindOne 查询当天的访客
+ * @param { Function } model 请求模型
+ * @param { Object } pm 查询参数
+ * @returns {Promise}
+ */
+exports.visitorFindOne = (model, pm) => {
+  return new Promise((resolve, reject) => {
+    model
+      .findOne({
+        where: {
+          [Op.and]: [
+            pm,
+            sequelize.where(
+              sequelize.fn("DATE", sequelize.col("createAt")), // 表对应的字段
+              // 取当前日期
+              sequelize.literal("CURRENT_DATE")
+            ),
+          ],
+        },
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+/**
+ * updateVisitorCount 更新当天的老访客
+ * @param { Function } model 请求参数
+ * @param { Object } pm 请求参数
+ * @param { String } ip ip地址
+ * @returns {Promise}
+ */
+
+exports.updateVisitorCount = async (model, pm, ip) => {
+  return new Promise((resolve, reject) => {
+    model
+      .update(pm, {
+        where: ip,
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
